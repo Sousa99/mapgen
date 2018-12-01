f6507e46137f054e52cbe629c02141811b64afb7
Rodrigo Sousa <rod_sousa@hotmail.com>
2018-12-01 00:08:24 +0000

    Added GradientTable and transtion specific Levels

--- biome.go
+++ biome.go
@@ -6,31 +6,58 @@ import (
 	"github.com/lucasb-eyer/go-colorful"
 )
 
-func getColor(transition bool, e float64, h1 string, c1e float64, h2 string, c2e float64) (colorful.Color, error) {
-	c1, errorValue := colorful.Hex(h1)
-	if transition {
-		c2, _ := colorful.Hex(h2)
-		return c1.BlendLab(c2, (e-c1e)/(c2e-c1e)).Clamped(), errorValue
+// This table contains the "keypoints" of the colorgradient you want to generate.
+// The position of each keypoint has to live in the range [0,1]
+type GradientTable []struct {
+	Color      colorful.Color
+	Pos        float64
+	Transition bool
+}
+
+// This is the meat of the gradient computation. It returns a LAB-blend between
+// the two colors around `t` respecting transition
+// Note: It relies heavily on the fact that the gradient keypoints are sorted.
+func (self GradientTable) GetInterpolatedColorFor(t float64) colorful.Color {
+	for i := 0; i < len(self)-1; i++ {
+		c1 := self[i]
+		c2 := self[i+1]
+		if c1.Pos <= t && t <= c2.Pos {
+			// We are in between c1 and c2. Go blend them!
+			if c1.Transition {
+				t := (t - c1.Pos) / (c2.Pos - c1.Pos)
+				return c1.Color.BlendLab(c2.Color, t).Clamped()
+			}
+
+			if t-c1.Pos < c2.Pos-t {
+				return c1.Color
+			} else {
+				return c2.Color
+			}
+		}
 	}
 
-	return c1, errorValue
+	return self[len(self)-1].Color
+}
+
+// This is a very nice thing Golang forces you to do!
+// It is necessary so that we can write out the literal of the colortable below.
+func MustParseHex(s string) colorful.Color {
+	c, _ := colorful.Hex(s)
+	return c
 }
 
 func biome(e float64, transition bool) (color.Color, error) {
-	// #2c52a0; #3766c8; #d0d080; #589619; #426220; #5c453e; #4d3b39; #ffffff
-	if e < 0.3 {
-		return getColor(transition, e, "#0a1778", 0, "#3766c8", 0.3)
-	} else if e < 0.4 {
-		return getColor(transition, e, "#3766c8", 0.3, "#d0d080", 0.4)
-	} else if e < 0.45 {
-		return getColor(transition, e, "#d0d080", 0.4, "#589619", 0.45)
-	} else if e < 0.60 {
-		return getColor(transition, e, "#589619", 0.45, "#5c453e", 0.60)
-	} else if e < 0.70 {
-		return getColor(transition, e, "#5c453e", 0.60, "#4d3b39", 0.70)
-	} else if e < 0.80 {
-		return getColor(transition, e, "#4d3b39", 0.70, "#ffffff", 0.84)
-	} else {
-		return colorful.Hex("#ffffff")
+
+	keypoints := GradientTable{
+		{MustParseHex("#0a1778"), 0.0, true},
+		{MustParseHex("#3766c8"), 0.3, false},
+		{MustParseHex("#d0d080"), 0.4, true},
+		{MustParseHex("#589619"), 0.45, true},
+		{MustParseHex("#5c453e"), 0.60, true},
+		{MustParseHex("#4d3b39"), 0.70, true},
+		{MustParseHex("#ffffff"), 0.80, true},
+		{MustParseHex("#ffffff"), 1, true},
 	}
+
+	return keypoints.GetInterpolatedColorFor(e), nil
 }
